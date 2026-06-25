e832a2ba0b13dd847a5b38684873514f52cdadc8
Shivani <arnukrm0@gmail.com>
2026-06-25 19:34:27 +0530

    updated regression suit

--- test-data/loginData.js
+++ test-data/loginData.js
@@ -1,6 +1,6 @@
 const loginData = {
     "standard_user":"standard_user",
-    "locked_out_user":"locked_out_user",
+   // "locked_out_user":"locked_out_user",
     "problem_user":"problem_user",
     "performance_glitch_user":"performance_glitch_user",
     "error_user":"error_user",
